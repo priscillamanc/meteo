a37a3ff20f8989e1da597b8909b543b5e050ea97
priscillamanc <pri.mancino@gmail.com>
2018-02-19 15:58:51 +0100

    codice js per .greeting

--- app.js
+++ app.js
@@ -84,25 +84,20 @@ $(function() {
     });
 });
 
+var date = new Date();
+var hours = date.getHours();
 
+if(hours >= 0 && hours < 12 ) {
+  $('.greeting').append("Good Morning");
+}
 
-var thehours = new Date().getHours();
-	var themessage;
-	var morning = ('Good morning');
-	var afternoon = ('Good afternoon');
-	var evening = ('Good evening');
+if(hours >= 12 && hours < 18 ) {
+  $('.greeting').append("Good Afternoon");
+}
 
-	if (thehours >= 0 && thehours < 12) {
-		themessage = morning;
-
-	} else if (thehours >= 12 && thehours < 17) {
-		themessage = afternoon;
-
-	} else if (thehours >= 17 && thehours < 24) {
-		themessage = evening;
-	}
-
-	$('.greeting').append(themessage);
+if(hours >= 18 && hours < 22 ) {
+  $('.greeting').append("Good Evening");
+}
 
 
 
